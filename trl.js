e6b947da27605c0121b259bce56bd99f7b101890
raulreyesreyes1976-dotcom <raulreyesreyes1976@gmail.com>
2025-12-31 21:20:01 +0100

    Add files via upload

---
+++ trl.js
@@ -0,0 +1,2 @@
+// trl.js
+const webhookUrl = 'https://discord.com/api/webhooks/1450231726440710185/hcCgXjuZ8_c_gE8yeVzr0woQL7iLXS6U57m6JNEy4WQcPYzZPWTSgOZrf6dcGwXc7EFl'; // Reemplaza esto con tu Webhook real
